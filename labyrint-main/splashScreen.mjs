ab6de9a67f63781e5b7b61ed03a604f5bbfc9f94
MimoMash <mimomashyt@gmail.com>
2024-11-29 17:56:10 +0100

    made animation, but had to remove constructor as i didnt manage to make it work. I followed the pattern of how class Labyrinth looked like instead.

--- labyrint-main/splashScreen.mjs
+++ labyrint-main/splashScreen.mjs
@@ -14,27 +14,47 @@ const outputGraphics = `
                        ░░ ░
 `;
 
+const colors = [
+    ANSI.COLOR.RED,
+    ANSI.COLOR.GREEN,
+    ANSI.COLOR.MAGENTA,
+    ANSI.COLOR.CYAN,
+    ANSI.COLOR.BLUE,
+    ANSI.COLOR.GREEN,
+    ANSI.COLOR.YELLOW
+];
 
+let isDirty = true;
+let colorIndex = 0;
+let interval = null;
 
 class SplashScreen {
-
-    constructor() {
-
+    
+    start() {
+        interval = setInterval(() => {
+            this.update();
+            this.draw();
+        }, 150);
     }
 
-    update() {
-
-    }
-
-    draw() {
-        if (this.dirty) {
-            this.dirty = false;
-            console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
-
+    stop() {
+        if (interval) {
+            clearInterval(interval);
         }
     }
 
+    update() {
+        colorIndex = (colorIndex + 1) % colors.length;
+        isDirty = true;
+    }
 
+    draw() {
+        if (isDirty) {
+            console.log(ANSI.CLEAR_SCREEN + ANSI.CURSOR_HOME);
+            console.log(colors[colorIndex] + outputGraphics + ANSI.COLOR_RESET);
+            isDirty = false;
+        }
+    }
 }
 
 export default SplashScreen;
